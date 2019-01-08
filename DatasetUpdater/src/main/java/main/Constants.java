113cb6141911deaae1ddf5588753ec693db5337e
burak <burakyonyul@gmail.com>
2019-01-08 18:37:03 +0300

    Artificial Data for datasets is being generated

---
+++ DatasetUpdater/src/main/java/main/Constants.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Constants {
+
+	private static String DBPEDIA_RSC_PREFIX="http://dbpedia.org/resource/";
+	private static String NYTIME_RSC_PREFIX="http://data.nytimes.com/";
+
+}
